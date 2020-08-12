0600fafcc81c4329acad56362b277537240000d6
giselleLago <gisellelago25994@gmail.com>
2020-08-12 18:59:21 +0200

    Add project files.

---
+++ CW_SimplePigLatin/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_SimplePigLatin
+{
+    public class Kata
+    {
+        public string PigIt(string str)
+        {
+            var list = str.Split(" ").ToList();
+            var newlist = new List<string>();
+            foreach (var item in list)
+            {
+                var x = item.ToCharArray();
+                var firstLetter = x[0];
+                var newItem = new char[0].ToList();
+                for (int i = 1; i < x.Length; i++)
+                {
+                    newItem.Add(x[i]);
+                }
+                newItem.Add(firstLetter);
+                newItem.Add('a');
+                newItem.Add('y');
+                newlist.Add(string.Join("" , newItem));
+            }
+            return string.Join(" ", newlist);
+        }
+    }
+}
---
+++ CW_SimplePigLatin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_SimplePigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.PigIt("Pig latin is cool"));
+            Console.ReadKey();
+        }
+    }
+}
